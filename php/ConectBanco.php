761a125c933ecaa1aee5adebd0575d3646bd06d5
Pedro R Hadad <89867431+MetalSlug7-pit@users.noreply.github.com>
2023-03-18 22:04:30 -0300

    Update ConectBanco.php

--- php/ConectBanco.php
+++ php/ConectBanco.php
@@ -1,11 +1,4 @@
 <?php
-	$servername = "localhost";
-	$user = "root";
-	$pass = "Pedro@1569733";
-	$dbname = "iGeekAnime";
-	try{
-		$conn = new PDO("mysql:host=$servername; port=3306; dbname=$dbname", $user, $pass);
-	}catch(PDOException $e){
-		echo "Connection failed: " . $e->getMessage();
-	}
+	railway connect mysql;
+	mysql -hcontainers-us-west-124.railway.app -uroot -pNgvY7wbKOzojXr4YLlcX --port 6833 --protocol=TCP railway;
 ?>
